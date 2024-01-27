c1ca8d4f6f56d0c9aa3376a804dd97a8ce654ee0
JayChauhan189 <jay189.chauhan@gmail.com>
2024-01-27 18:12:26 +0530

    [Updated]: Login API integrated with Frontend

---
+++ React/doubtbuddy24x7/src/components/Doubtsolver/Onlinestatus.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const OnlineStatus = () => {
+  const [onlineStatus, setOnlineStatus] = useState(true);
+  // Function to handle DoubtAsker click
+  const handleDoubtAsker = async () => {
+    console.log("Hello");
+    setOnlineStatus(false);
+    try {
+      await axios.get(`http://localhost:3003/user/status/false`);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // Function to handle DoubtSolver click
+  const handleDoubtSolver = async () => {
+    setOnlineStatus(true);
+    try {
+      await axios.get(`http://localhost:3003/user/status/true`);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return (
+    <div>
+      <h1>Online Status</h1>
+      <button onClick={handleDoubtAsker}>DoubtAsker{onlineStatus} {console.log(onlineStatus)}</button>
+      <button onClick={handleDoubtSolver}>DoubtSolver {onlineStatus} {console.log(onlineStatus)}</button>
+    </div>
+  );
+};
+
+
+export default OnlineStatus;
+
+
--- React/doubtbuddy24x7/src/components/Home.js
+++ React/doubtbuddy24x7/src/components/Home.js
@@ -1,29 +1,61 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './NavbarComp'
 import { Button, Col, Container, Row } from 'react-bootstrap'
+import { useLocation } from 'react-router-dom';
 export default function Home(props) {
-  {console.log(props.user);}
+  const location = useLocation();
+  const { user } = location.state || {};
+  
+  let trimmeduser = user.name.split(" ").join("");
+  console.log(trimmeduser);
+
+
+  useEffect(() => {
+    async function adduser(){
+      try {
+        const url = "http://localhost:3003/user/login";
+        let result = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            "username":user.name,
+            "email":trimmeduser+"@gmail.com"
+        })
+        });
+        result = await result.json()
+        //
+        if (result) {
+          console.log(result);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    adduser();
+  }, []);
   return (
     <>
-    <Navbar/>
-    
-    <Container className="vh-100 d-flex flex-column justify-content-center align-items-center">
-    <Row className="mb-4">
-        <Col>
-          <h1 className="text-center d-inline">Welcome to DoubtBuddy!.</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="text-center">
-          <Button variant="primary" className="mx-4 px-5 py-3" size='lg'>
-            Ask a Doubt?
-          </Button>
-          <Button variant="primary" className=' px-5 py-3' size='lg'>
-            Solve a Doubt?
-          </Button>
-        </Col>
-      </Row>
-    </Container>
-  </>
+      <Navbar />
+      <Container className="vh-100 d-flex flex-column justify-content-center align-items-center">
+        <Row className="mb-4">
+          <Col>
+            <h1 className="text-center d-inline">Welcome to DoubtBuddy!.</h1>
+          </Col>
+        </Row>
+        <Row>
+
+          <Col className="text-center">
+            <Button variant="primary" className="mx-4 px-5 py-3" size='lg'>
+              Ask a Doubt?
+            </Button>
+            <Button variant="primary" className=' px-5 py-3' size='lg'>
+              Solve a Doubt?
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
