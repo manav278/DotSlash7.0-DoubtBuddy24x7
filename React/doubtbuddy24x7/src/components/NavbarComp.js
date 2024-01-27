67b09226932cf72dc1ae1859335792bbacde0165
JayChauhan189 <jay189.chauhan@gmail.com>
2024-01-28 05:18:34 +0530

    [Update]: Range Slider, Minor Change in VideoConf Module

--- React/doubtbuddy24x7/src/components/NavbarComp.js
+++ React/doubtbuddy24x7/src/components/NavbarComp.js
@@ -3,28 +3,28 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from 'react'
-import { Button } from 'react-bootstrap';
-
+import { Button, } from 'react-bootstrap';
 export default function NavbarComp() {
     return (
         <div>
+            
             <Navbar bg="dark" variant="dark" expand="lg">
-            <div className='container-fluid mx-2'>
-                <Navbar.Brand href="#" className='h1'>DoubtBuddy24*7</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    {/* <Nav className="mr-auto">
+                <div className='container-fluid mx-2'>
+                    <Navbar.Brand href="/" className='h1'>DoubtBuddy24*7</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        {/* <Nav className="mr-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#features">Features</Nav.Link>
                         <Nav.Link href="#pricing">Pricing</Nav.Link>
                     </Nav> */}
-                    {/* <Button variant="secondary">Secondary</Button> */}
-                </Navbar.Collapse>
+                        {/* <Button variant="secondary">Secondary</Button> */}
+                    </Navbar.Collapse>
                 </div>
                 <div className='mx-3'>
-                <Button className='px-5 text' variant="outline-primary">UserProfile</Button>
-      
-                    </div>    
+                    <Button className='px-5 text' variant="outline-primary">UserProfile</Button>
+
+                </div>
             </Navbar>
         </div>
     )
