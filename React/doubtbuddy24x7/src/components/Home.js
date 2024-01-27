63482c29cc6f39f1a5d581178de21c9c57a48b93
JayChauhan189 <jay189.chauhan@gmail.com>
2024-01-27 14:42:25 +0530

    [Added]: HomePage, VideoConferencePage

---
+++ React/doubtbuddy24x7/src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './NavbarComp'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    
+    <Container className="vh-100 d-flex flex-column justify-content-center align-items-center">
+    <Row className="mb-4">
+        <Col>
+          <h1 className="text-center d-inline">Welcome to DoubtBuddy!.</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <Button variant="primary" className="mx-4 px-5 py-3" size='lg'>
+            Ask a Doubt?
+          </Button>
+          <Button variant="primary" className=' px-5 py-3' size='lg'>
+            Solve a Doubt?
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  </>
+  )
+}
--- React/doubtbuddy24x7/src/index.js
+++ React/doubtbuddy24x7/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import $ from 'jquery'; 
+import Popper from 'popper.js'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import VideoConference from './components/VideoConference';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <VideoConference/>
   </React.StrictMode>
 );
 
